9e1a8c1b65766aaa51d56e0f87acd6f060e30ed5
gc2hnik <gc2hnik@AP710303>
2017-05-03 11:34:15 +0200

    app components html template created, track list component added

--- RollerTrax.Web/src/app/app.module.ts
+++ RollerTrax.Web/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component'
 import { HeroService } from './hero.service'
 import { DashboardComponent } from './dashboard.component'
-
-import { AppRoutingModule } from './app-routing.module';
+import { TrackList } from './track-list.component'
 
 @NgModule({
 
@@ -24,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
         AppComponent,
         DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
+        TrackList
     ],
 
     bootstrap: [AppComponent],
---
+++ RollerTrax.Web/src/app/track-list.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'tracklist',
+    moduleId: module.id,
+    templateUrl: './track-list.component.html',
+    styleUrls: ['./track-list.component.css']
+})
+export class TrackList implements OnInit {
+
+    ngOnInit(): void {
+ 
+    }
+
+}
